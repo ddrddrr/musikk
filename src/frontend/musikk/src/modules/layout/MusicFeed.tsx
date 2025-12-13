28266f91de9393cf90338270e4ee97c69c76d9b8
Kirill Vorozhtsov <Kirill.Vorozhtsov@adastragrp.com>
2025-12-13 12:59:09 +0100

    fix: add staletime and change multiple usequery to single usequeries in musicfeed

--- src/frontend/musikk/src/modules/layout/MusicFeed.tsx
+++ src/frontend/musikk/src/modules/layout/MusicFeed.tsx
@@ -1,32 +1,37 @@
+import { PaginatedRes } from "@/api/types";
 import { fetchCollections } from "@/modules/song-collections/api/queries";
 import { SongCollectionCarousel } from "@/modules/song-collections/SongCollectionCarousel";
-import { useQuery } from "@tanstack/react-query";
+import { Collection } from "@/modules/song-collections/types";
+import { useQueries } from "@tanstack/react-query";
 
 export function MusicFeed() {
-    const playlists = useQuery({
-        queryKey: ["collections.playlists.latest"],
-        queryFn: () => fetchCollections({ type: "playlist" }),
-    });
-
-    const albums = useQuery({
-        queryKey: ["collections.albums.latest"],
-        queryFn: () => fetchCollections({ type: "album" }),
-    });
-
-    const followedCollections = useQuery({
-        queryKey: ["collections.followed.latest"],
-        queryFn: () =>
-            fetchCollections({
-                connection: "followed",
-            }),
-    });
-
-    const friendsCollections = useQuery({
-        queryKey: ["collections.friends.latest"],
-        queryFn: () =>
-            fetchCollections({
-                connection: "friends",
-            }),
+    const [playlists, albums, followedCollections, friendsCollections] = useQueries({
+        queries: [
+            {
+                queryKey: ["collections", "playlists", "latest"],
+                queryFn: () => fetchCollections({ type: "playlist" }),
+                select: (data: PaginatedRes<Collection>) => data.results,
+                staleTime: 60_000,
+            },
+            {
+                queryKey: ["collections", "albums", "latest"],
+                queryFn: () => fetchCollections({ type: "album" }),
+                select: (data: PaginatedRes<Collection>) => data.results,
+                staleTime: 60_000,
+            },
+            {
+                queryKey: ["collections", "followed", "latest"],
+                queryFn: () => fetchCollections({ connection: "followed" }),
+                select: (data: PaginatedRes<Collection>) => data.results,
+                staleTime: 30_000,
+            },
+            {
+                queryKey: ["collections", "friends", "latest"],
+                queryFn: () => fetchCollections({ connection: "friends" }),
+                select: (data: PaginatedRes<Collection>) => data.results,
+                staleTime: 30_000,
+            },
+        ],
     });
 
     const isLoading =
@@ -34,6 +39,7 @@ export function MusicFeed() {
         albums.isPending ||
         followedCollections.isPending ||
         friendsCollections.isPending;
+
     const isError =
         playlists.isError ||
         albums.isError ||
@@ -45,17 +51,17 @@ export function MusicFeed() {
 
     return (
         <div className="flex flex-col gap-8">
-            <SongCollectionCarousel collections={playlists.data?.results} title="New Playlists" />
+            <SongCollectionCarousel collections={playlists.data} title="New Playlists" />
 
-            <SongCollectionCarousel collections={albums.data?.results} title="New Albums" />
+            <SongCollectionCarousel collections={albums.data} title="New Albums" />
 
             <SongCollectionCarousel
-                collections={followedCollections.data?.results}
+                collections={followedCollections.data}
                 title="Latest Added by Followed Users"
             />
 
             <SongCollectionCarousel
-                collections={friendsCollections.data?.results}
+                collections={friendsCollections.data}
                 title="Latest Added by Friends"
             />
         </div>
