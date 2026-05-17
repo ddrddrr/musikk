52ffd404f473235c29d5284e4ae2c4fa582d0623
Kirill Vorozhtsov <Kirill.Vorozhtsov@adastragrp.com>
2026-05-17 13:53:31 +0200

    fix: make safari on macs use DASH, adjust comments/text to explain ios safari issues with web audio api

--- src/frontend/musikk/src/features/player/getUrlForSong.ts
+++ src/frontend/musikk/src/features/player/getUrlForSong.ts
@@ -1,8 +1,11 @@
 import { CollectionSong } from "@/features/collections/types.ts";
 
+// maxTouchPoints distinguishes iPad (>=2) from desktop Mac (0)
+// as it turned out the older "ontouchend in document" check is also true on desktop Safari and so
+// detected Macs by a mistake
 const isIOS =
     /iPad|iPhone|iPod/.test(navigator.userAgent) ||
-    (navigator.userAgent.includes("Mac") && "ontouchend" in document);
+    (navigator.userAgent.includes("Mac") && navigator.maxTouchPoints > 1);
 
 export function getUrlForSong(collectionSong: CollectionSong): string | undefined {
     return isIOS ? collectionSong.song.m3u8 : collectionSong.song.mpd;
